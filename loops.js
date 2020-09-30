5295dcf18244e57597c2bb92eba68f0da77b9190
Aleksey <fduchunru@gmail.com>
2020-09-30 16:16:30 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,4 +18,15 @@ function whileLoop(n){
 
 return "done";
 }
-//whileLoop(6)
+
+let i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+function doWhileLoop(num){
+ do {
+   console.log("I run once regardless.");
+ } while (incrementVariable() < num);
+ }
