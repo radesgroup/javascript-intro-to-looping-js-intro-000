f4dac5dfc96511ae4312219eb87cb29cd1737ada
Aleksey <fduchunru@gmail.com>
2020-09-30 14:55:44 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++)
+  {
+
+  array.push( `I am ${i} strange loop${i === 1 ? '' : 's'}.`);
+
+  }
+  return array;
+}
+forLoop()
+
+var n = 5;
+function whileLoop(n){
+  while (n > 0) {
+    console.log(n-1)
+  }
+console.log("done")
+return n
+}
